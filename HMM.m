aa1575cdd60f2e0e950f7118b28858da114b4986
Faruk8502 <90871155+Faruk8502@users.noreply.github.com>
2021-11-28 03:37:17 +0300

    Update HMM.m

--- HMM.m
+++ HMM.m
@@ -1,22 +1,20 @@
-function [A,B,pi]= HMM(y,T,NItterations)
-s=0; s1=0; s2=0; s3=0; num=0;
-for I=1:NItterations
-O=[1 2 3];
+% function [A,B,pi]= HMM1(W, AS, Splain_b1, Splain_b2, NItterations)
+for I=100:NItterations
 N=3;
-M=3;
-pi=randn(1,M);
-A=randn(N,M);
-B=randn(N,M);
-for t=1:T
+pi=randn(1,N);
+A=randn(N,N);
+for t=2:T
   for i=1:3
-    if t==1
-      alpha(i,t)=pi(i)*B(y(t),i);
+    if t==2
+      y=abs(round(abs(W(i,t))-abs(W(i,t-1))))*10^3+1;
+      alpha(i,t)=pi(i)*B(i,y);
     else
         for j=1:3
          s1(j)=alpha(j,t-1)*A(i,j);
         end
       s2=sum(s1);
-      alpha(i,t)=B(y(i),i)*s2; 
+      y=abs(round(abs(W(i,t))-abs(W(i,t-1))))*10^3+1;
+      alpha(i,t)=B(i,y)*s2; 
     end       
   end
 end
@@ -26,7 +24,8 @@ betta(1:3,T)=1;
 for k=1:(T-1)
    for i=1:3
        for j=1:3
-       s(j)=betta(j,T-k+1)*A(i,j)*B(y(T-k+1),j);
+       y=abs(round(abs(W(i,T-k+1))-abs(W(i,T-k))))*10^3+1;
+       s(j)=betta(j,T-k+1)*A(i,j)*B(j,y);
        end
    betta(i,T-k)=sum(s);
    end
@@ -45,7 +44,8 @@ end
 for t=1:T-1
     for i=1:3
        for j=1:3
-          s1(i,j)=alpha(i,t)*betta(j,t+1)*A(i,j)*B(y(t+1),j);
+          y=abs(round(abs(W(i,t+1))-abs(W(i,t))))*10^3+1;
+          s1(i,j)=alpha(i,t)*betta(j,t+1)*A(i,j)*B(j,y);
        end
     end
 s2=sum(s1,2);
@@ -53,17 +53,20 @@ s3=sum(s2,1);
 
     for i=1:3
        for j=1:3
-          num=alpha(i,t)*betta(j,t+1)*A(i,j)*B(y(t+1),j);  
+          y=abs(round(abs(W(i,t+1))-abs(W(i,t))))*10^3+1;
+          num=alpha(i,t)*betta(j,t+1)*A(i,j)*B(j,y);  
           psi(i,t)=num/s3;
        end
     end
 end
-
+k=1;
 for i=1:3
       pi(i)= gamma(i,1);
-      for t=1:10
-         if y(t)==O(i)
+      for t=2:T
+      y=abs(round(abs(W(i,t))-abs(W(i,t-1))));
+         if y==O(i,k)
              delta=1;
+             k=k+1;
          else
              delta=0;
          end
@@ -74,4 +77,6 @@ num=sum(s2,1);
 B(i)=num/sum(gamma(i,1:T),2);
 end
 A=sum(psi,2)/sum(gamma,2);
+clear s s1 s2 s3 num %инициализировать в начале
 end
+% end
